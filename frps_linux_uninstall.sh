7076a914abd4bffd635acd1dd4272fa4169b015b
sdf8057 <54014465+sdf8057@users.noreply.github.com>
2021-01-15 15:57:50 +0800

    Update frps_linux_uninstall.sh

--- frps_linux_uninstall.sh
+++ frps_linux_uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # 停止frps
-sudo systemctl stop frps && \
+systemctl stop frps && \
 # 删除frps
 rm -rf /usr/local/frp && \
 # 删除frps.service
 rm -rf /lib/systemd/system/frps.service && \
-sudo systemctl daemon-reload && \
+systemctl daemon-reload && \
 # 删除本文件
 rm -rf frps_linux_uninstall.sh
 echo "============================" &&\
